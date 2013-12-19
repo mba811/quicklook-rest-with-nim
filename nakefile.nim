1068d761bfed27610593bc1646e01150049b1c76
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-12-19 23:50:07 +0100

    Modifies nakefile to use own rst generator. Refs #6.

--- nakefile.nim
+++ nakefile.nim
@@ -1,8 +1,24 @@
-import nake, os, times, osproc, htmlparser, xmltree, strtabs, strutils
+import nake, os, times, osproc, htmlparser, xmltree, strtabs, strutils,
+  rester
 
 let
   rst_files = @["docs"/"debugging_quicklook", "docs"/"release_steps",
     "docs"/"CHANGES", "LICENSE", "README", "docindex"]
+
+proc rst2html(filename: string): bool =
+  let output = safe_rst_file_to_html(filename)
+  if output.len > 0:
+    writeFile(filename.changeFileExt("html"), output)
+    result = true
+  #var d: TRstGenerator
+  #initRstGenerator(d, outHtml, config, filename,
+  #  {roSupportRawDirective}, nil, rst.defaultMsgHandler)
+  #var
+  #  dummyHasToc = false
+  #  rst = rstParse(s, filename, 0, 1, dummyHasToc, options)
+  #  result = ""
+  #renderRstToOut(d, rst, result)
+  #writeFile(filename.changeFileExt("html"), result)
 
 proc change_rst_links_to_html(html_file: string) =
   ## Opens the file, iterates hrefs and changes them to .html if they are .rst.
@@ -49,7 +65,7 @@ task "doc", "Generates export API docs for for the modules":
   # Generate html files from the rst docs.
   for rst_file, html_file in all_rst_files():
     if not html_file.needs_refresh(rst_file): continue
-    if not shell("nimrod rst2html --verbosity:0", rst_file):
+    if not rst2html(rst_file):
       quit("Could not generate html doc for " & rst_file)
     else:
       change_rst_links_to_html(html_file)
@@ -63,3 +79,10 @@ task "check_doc", "Validates rst format for a subset of documentation":
     if output.len > 0 or exit != 0:
       echo "Failed python processing of " & rst_file
       echo output
+
+task "clean", "Removes temporal files, mainly":
+  for path in walkDirRec("."):
+    let (dir, name, ext) = splitFile(path)
+    if ext == ".html":
+      echo "Removing ", path
+      path.removeFile()
